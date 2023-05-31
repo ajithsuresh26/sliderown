722d29b6391a5fb7b575800353ed2d247fc9f812
ajithsuresh26 <134696503+ajithsuresh26@users.noreply.github.com>
2023-05-31 13:34:06 +0530

    Add files via upload

---
+++ Slider Learn/script.js
@@ -0,0 +1,38 @@
+function circle1(){
+    document.querySelector(".circle1").style.width='40px';
+    document.querySelector(".circle1").style.background='red';
+    document.querySelector(".circle2").style.width='15px';
+    document.querySelector(".circle2").style.background='blue';
+    document.querySelector(".circle3").style.width='15px';
+    document.querySelector(".circle3").style.background='blue';
+}
+
+function circle2(){
+    document.querySelector(".circle1").style.width='15px';
+    document.querySelector(".circle1").style.background='blue';
+    document.querySelector(".circle2").style.width='40px';
+    document.querySelector(".circle2").style.background='red';
+    document.querySelector(".circle3").style.width='15px';
+    document.querySelector(".circle3").style.background='blue';
+}
+
+function circle3(){
+    document.querySelector(".circle1").style.width='15px';
+    document.querySelector(".circle1").style.background='blue';
+    document.querySelector(".circle2").style.width='15px';
+    document.querySelector(".circle2").style.background='blue';
+    document.querySelector(".circle3").style.width='40px';
+    document.querySelector(".circle3").style.background='red';
+}
+
+function secoundSlide(){
+   document.querySelector(".card-group").style.transform='translateX(-1017px)';
+}
+
+function firstSlide(){
+    document.querySelector(".card-group").style.transform='translateX(0)';
+ }
+
+ function thirdSlide(){
+    document.querySelector(".card-group").style.transform='translateX(-2034px)';
+ }
